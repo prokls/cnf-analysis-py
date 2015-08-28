1ed5c214fa76d9744bd4d35b06864b9b78891abf
prokls <lukas.prokop@student.tugraz.at>
2015-08-28 14:37:28 +0200

    Fix documentation for combine. Improve combine.

--- combine.py
+++ combine.py
@@ -6,13 +6,17 @@
 
     Given several metrics specification files (eg. JSON here) such as::
 
-        {"metric":{"count_unique_variables": 13408,"count_existential_literals": 0, …}}
+        {"metrics":
+          {"metric":{"count_unique_variables": 13408,"count_existential_literals": 0, …}}
+        }
 
     combine them into one metrics specification file::
 
         {"metrics":
           {"metric":{"count_unique_variables": 13408,"count_existential_literals": 0, …}},
-          {"metric":{"count_unique_variables": 1283,"count_existential_literals": 0, …}}}
+          {"metric":{"count_unique_variables": 1283,"count_existential_literals": 0, …}}},
+          …
+        }
 
     (C) 2015, BSD 3-clause licensed, Lukas Prokop
 """
@@ -26,14 +30,74 @@ import xml.dom.minidom
 import xml.sax.saxutils
 import xml.etree.ElementTree
 
-import processing
+import statsfile
 
-METRICS = processing.METRICS
-BATCH_SIZE = 50
 
-Metrics = collections.namedtuple("Metrics",
-    [key for key, value in METRICS.items() if value[0]]
-)
+class XMLWriter:
+    def __init__(self, filepath, encoding='utf-8'):
+        self.filepath = filepath
+        self.encoding = encoding
+
+    def __enter__(self):
+        self.fp = open(self.filepath, 'wb')
+        content = self.fp.read()
+
+        if self.input_format.lower() == 'xml':
+            self.metrics = from_xml_file(content, self.input_encoding)
+        else:
+            self.metrics = from_json_file(content, self.input_encoding)
+
+        return self.metrics
+
+    def __exit__(self, type, value, traceback):
+        failed = (traceback is not None)
+        try:
+            self.fp.close()
+        except Exception:
+            failed = True
+
+        if self.output_format == 'xml':
+            to_xml(self.metrics, self.output_file, self.output_encoding)
+        else:
+            to_json(self.metrics, self.output_file, self.output_encoding)
+
+        if failed:
+            print('FAIL ' + self.output_file, file=sys.stderr)
+        else:
+            print('OKAY ' + self.output_file, file=sys.stderr)
+        return True
+
+
+
+class JSONWriter:
+    def __init__(self, filepath, encoding='utf-8'):
+        self.filepath = filepath
+        self.first_object = True
+        self.encoding = encoding
+
+    def __enter__(self):
+        self.fp = open(self.filepath, 'wb')
+        self.fp.write(b'{\n  "metrics": [\n')
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.fp.write(b'\n  ]\n}\n')
+        self.fp.close()
+
+        print('OKAY ' + self.filepath, file=sys.stderr)
+        return True
+
+    def write(self, meta: dict, metric: dict):
+        if not self.first_object:
+            self.fp.write(b',\n')
+        obj = meta
+        obj['metric'] = metric
+        strrepr = json.dumps(obj, indent=2, sort_keys=True).replace('\n', '    ')
+        self.fp.write(strrepr.encode(self.encoding))
+        self.first_object = False
+
+
+
 
 
 def read_xml_file(blob: bytes, encoding: str) -> (str, str, str, Metrics):
@@ -120,18 +184,26 @@ def to_json(filepath: str, metrics: [(str, str, str, Metrics)], encoding: str):
             json.dump(obj, fp, sort_keys=True, indent=2).encode(encoding)
 
 
+
 def main(args: argparse.Namespace) -> int:
-    # TODO: use batchwise cache such that at most BATCH_SIZE instances are stored
-    cache = []
-
-    for i, inputfile in enumerate(args.inputfiles):
-        data = read_single_file(inputfile, args.input_encoding)
-        cache.append(data)
-
-    if args.format == 'xml':
-        to_xml(args.output, cache, args.output_encoding)
+    """Main routine."""
+    if args.output_format == 'json':
+        Writer = JSONWriter
     else:
-        to_json(args.output, cache, args.output_encoding)
+        Writer = XMLWriter
+
+    with Writer(args.outfile, encoding=args.output_encoding) as writer:
+        for inputfile in args.inputfiles:
+            if statsfile.detect_format(inputfile) == 'xml':
+                Reader = XMLReader
+            else:
+                Reader = JSONReader
+
+            with Reader(inputfile) as reader:
+                for (meta, metric) in reader:
+                    writer.write(meta, metric)
+
+    return 0
 
 
 if __name__ == '__main__':
@@ -139,13 +211,13 @@ if __name__ == '__main__':
 
     parser.add_argument('inputfiles', metavar='inputfiles', nargs='+',
                         help='files to read metrics from and combine')
-    parser.add_argument('-o', '--output', dest='output', default='-',
+    parser.add_argument('-o', '--output', dest='outputfile', default='-',
                         help='output filepath (default: stdout)')
     parser.add_argument('-u', '--input-encoding', dest='input_encoding', default='utf-8',
-                        help='input encoding (default: utf-8)')
+                        help='input encoding for all files (default: utf-8)')
     parser.add_argument('-e', '--output-encoding', dest='output_encoding', default='utf-8',
                         help='output encoding (default: utf-8)')
-    parser.add_argument('-f', '--format', default='json',
+    parser.add_argument('-f', '--output-format', dest='output_format', default='json',
                         help='output format (default: json)')
 
     args = parser.parse_args()
